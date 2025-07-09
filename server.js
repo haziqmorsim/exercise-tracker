4a52e0a6dac797de04bfb587d943473859d4cce3
Haziq Morsim <haziqmorsim@gmail.com>
2025-07-09 12:53:15 +0800

    Updated server.js

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+console.log('MONGO_URI from env:', process.env.MONGO_URI);
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✅ Connected to MongoDB'))
   .catch(err => console.error('❌ MongoDB connection error:', err));
