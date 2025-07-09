39b8ac367e06bf2a5fbf831751ad49f32aca1566
Haziq Morsim <haziqmorsim@gmail.com>
2025-07-09 23:49:17 +0800

    Updated files

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../sequelize');
+
+const User = sequelize.define('User', {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
+
+module.exports = User;
